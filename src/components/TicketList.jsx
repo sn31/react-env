267a5fbecfbc99cdcf62618351418fb2a2af75ed
SkyeN <skye@dames.es>
2018-11-13 16:25:52 -0800

    loop to create more than one tickets

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import Ticket from "./Ticket";
 
 function TicketList() {
+  const TicketData = {
+    locations: ['A1','C3','B4'],
+    names: ['Skye','Meria','Ryan'],
+    issues: ['Stuff is not working','bla','woooah']
+  }
+  let tickets = []
+  for(let i =0;i<TicketData.locations.length;i++)
+  {
+    tickets.push(
+      <Ticket
+      location={TicketData.locations[i]}
+      names={TicketData.names[i]}
+      issue={TicketData.issues[i]}
+    />)
+  }
   return (
-    <Ticket
-      location="3A"
-      names="Thato and Haley"
-      issue="Firebase will not save record!"
-    />
+    <div>
+      {tickets}
+    </div>
   );
 }
 
