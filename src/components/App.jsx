1e51652fd8ea09656c6c2bfab4c2d751b60c96bc
SkyeN <skye@dames.es>
2018-11-15 09:44:17 -0800

    add react-router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Header from './Header'
 import TicketList from './TicketList'
+import {Switch, Route} from 'react-router-dom'
+import NewTicketForm from './NewTicketForm'
+
 function App() {
   return (
     <div>
       <Header />
-      <TicketList/>
+      <Switch>
+        <Route exact path='/' component={TicketList} />
+        <Route exact path='/newticket' component={NewTicketForm} />
+      </Switch>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,17 @@
-import React from "react";
-
+import React from 'react'
+import { Link } from 'react-router-dom';
 function Header() {
   return (
     <div>
       <h1>Help Queue</h1>
+      <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
       <style jsx>{`
       div {
         margin-left: 1em;
       }
       `}</style>
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
