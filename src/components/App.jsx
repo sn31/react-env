e240c663438d49dc58779d1736a10f9a859a5356
SkyeN <skye@dames.es>
2018-11-25 18:57:30 -0800

    add Error404 component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Header from './Header'
 import TicketList from './TicketList'
 import {Switch, Route} from 'react-router-dom'
 import NewTicketForm from './NewTicketForm'
+import Error404 from './Error404'
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={TicketList} />
-        <Route exact path='/newticket' component={NewTicketForm} />
+        <Route path='/newticket' component={NewTicketForm} />
+        <Route component={Error404}/>
       </Switch>
     </div>
   )
