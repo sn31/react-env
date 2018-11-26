39054f07d93261abc67e26ad5a373caf3c405610
SkyeN <skye@dames.es>
2018-11-25 19:16:27 -0800

    add completed Error404 component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './Header'
 import TicketList from './TicketList'
 import {Switch, Route} from 'react-router-dom'
-import NewTicketForm from './NewTicketForm'
+import NewTicketControl from './NewTicketControl'
 import Error404 from './Error404'
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketForm} />
+        <Route path='/newticket' component={NewTicketControl} />
         <Route component={Error404}/>
       </Switch>
     </div>
