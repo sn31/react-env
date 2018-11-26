8fa324ddcc8d471d5a6832f9cbc828de82d7ac60
SkyeN <skye@dames.es>
2018-11-26 10:06:16 -0800

    add state and lift state

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import ConfirmationQuestions from './ConfirmationQuestions';
+import NewTicketForm from './NewTicketForm';
+import PropTypes from "prop-types";
 
 class NewTicketControl extends React.Component {
     constructor(props){
@@ -6,14 +9,30 @@ class NewTicketControl extends React.Component {
         this.state={
             formVisibleOnPage:false
         };
+        this.handleTroubleshootingConfirmation = this.handleTroubleshootingConfirmation.bind(this);
+    }
+    handleTroubleshootingConfirmation() {
+        this.setState({formVisibleOnPage:true});
+        
     }
     render() {
+        let currentlyVisibleContent = null;
+        if (this.state.formVisibleOnPage){
+            currentlyVisibleContent = <NewTicketForm onNewTicketCreation={this.props.onNewTicketCreation}/>;
+        }
+        else{
+            currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConfirmation}/>
+        }
         return (
-            <div>
-            <p>This is the NewTicketControl component</p>
-            </div>
+          <div>
+              {currentlyVisibleContent}
+          </div>
         );
     }
 }
+
+NewTicketControl.propTypes = {
+    onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketControl;
