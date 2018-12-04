d06d6661b990d4f63b5bd9e1e5379d2048424512
SkyeN <skye@dames.es>
2018-12-04 11:17:06 -0800

    add a reducer to hanle selecting a ticket

---
+++ __tests__/reducers/selected-ticket-reducer.test.js
@@ -0,0 +1,11 @@
+import selectedticketReducer from "./../../src/reducers/selected-ticket-reducer";
+
+describe("selectedTicketReducer", () => {
+  test("Should return default state if no action type is recognized", () => {
+    expect(selectedticketReducer({}, { type: null })).toEqual({});
+  });
+
+  test("Should record which ticket has been selected by user", () => {
+      expect(selectedticketReducer({},{type:'SELECT_TICKET',ticketId:1})).toEqual(1);
+  });
+});
