ff74ab6078460096a421b012f8e3b00357c6c1ea
SkyeN <skye@dames.es>
2018-12-04 10:02:46 -0800

    add Jest tests and reducer

--- __tests__/reducers/ticket-list-reducer.test.js
+++ __tests__/reducers/ticket-list-reducer.test.js
@@ -1,7 +1,36 @@
-import ticketListReducer from './../../src/reducers/ticket-list-reducer';
+import ticketListReducer from "./../../src/reducers/ticket-list-reducer";
 
-describe('ticketListReducer',() =>{
-    test('Should return default state if no action is regconized',()=>{
-        expect(ticketListReducer({},{type:null})).toBe({});
-    })
-})
+describe("ticketListReducer", () => {
+  let action;
+  const sampleTicketData = {
+    names: "Ryan & Aimen",
+    location: "4b",
+    issue: "Jest is being a diva and won't work with Webpack!",
+    timeOpen: 1500000000000,
+    id: 0
+  };
+  test("Should return default state if no action is regconized", () => {
+    expect(ticketListReducer({}, { type: null })).toBeNull;
+  });
+
+  test("Should successfully add new ticket data to masterTicketList", () => {
+    const { names, location, issue, timeOpen, id } = sampleTicketData;
+    action = {
+      type: "ADD_TICKET",
+      names: names,
+      location: location,
+      issue: issue,
+      timeOpen: timeOpen,
+      id: id
+    };
+    expect(ticketListReducer({}, action)).toEqual({
+      [id]: {
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id
+      }
+    });
+  });
+});
